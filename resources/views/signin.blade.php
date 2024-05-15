10c697de77ef354a2d21a0d135b822a82915de65
alwiismail66 <alwiismail66@gmail.com>
2024-05-15 09:42:36 +0700

    add signin layout

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -1,3 +1,29 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+    <title>Sign In</title>
+</head>
+
+<body>
+    <main>
+        <header>
+            <h1>welcome to my to do list</h1>
+            <p>login to your account</p>
+        </header>
+        <section>
+            <form action="" name="signup" target="">
+                <input type="email" id="email" name="email" placeholder="Email" maxlength="30" required>
+                <input type="password" id="password" name="password" placeholder="Password" maxlength="30" required>
+                <button>Sign In</button>
+            </form>
+            <p>Don't hava an account ? <a href="{{ route('signup') }}">Sign Up</a></p>
+        </section>
+    </main>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
+
+Route::get('/signup', function () {
+    return view('signup');
+})->name('signup');
+Route::get('/signin', function () {
+    return view('signin');
+})->name('signin');
